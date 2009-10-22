da7d84683f20d69a902f05d24a11cfd5bc139564
Pelayo <pelayo@static-host.(none)>
2009-10-22 16:00:15 -0300

    Added relative paths to settings.py and added the basic directory layout, leaving placeholder files until we get actual content in there.

--- kaariok/settings.py
+++ kaariok/settings.py
@@ -1,5 +1,10 @@
 # Django settings for kaariok project.
 
+#Build paths
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+REPO_ROOT = os.path.split(SITE_ROOT)[0]
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -35,12 +40,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(REPO_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -69,6 +74,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
