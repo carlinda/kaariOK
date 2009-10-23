8f7989b75d8605f65293dba47906ea3140548aa9
Pelayo Besa <pelayobesa+git@gmail.com>
2009-10-23 18:25:45 -0300

    Got the development url to serve static media up and running.

--- kaariok/urls.py
+++ kaariok/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls.defaults import *
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+# For testing only
+from django.shortcuts import render_to_response
+def test_base(request):
+    return render_to_response('test.html')
+# End testing
 
 urlpatterns = patterns('',
     # Example:
@@ -14,4 +21,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    # Serving media for 
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+    	{'document_root': settings.MEDIA_ROOT}),
+    
+    # For testing only
+    (r'^basetest/', test_base),
 )
