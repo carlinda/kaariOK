8c78d6c04f7337fde01f4b0d291e55e1ae578b8a
Pelayo Besa <pelayobesa+git@gmail.com>
2009-10-24 15:49:36 -0300

    - View now does the login.

--- kaariok/users/views.py
+++ kaariok/users/views.py
@@ -1,11 +1,23 @@
+from django.contrib.auth import authenticate, login
 from django.utils import simplejson
 from django.http import HttpResponse
 
-
+from django.contrib.auth.models import User
 
 def login_user(request, user_id):
     
+    user = User.objects.get(id=user_id)
+    login_user = authenticate(username=user.username, password=' ')
+    success = False
+    selected_id = user_id
+    if login_user is not None:
+        if user.is_active:
+            login(request, login_user)
+            success = True
+            selected_id = login_user.id
+            
     output = {
-        'success' : False,
+        'success' : success,
+        'user_id' : selected_id,
     }
     return HttpResponse(simplejson.dumps(output))
