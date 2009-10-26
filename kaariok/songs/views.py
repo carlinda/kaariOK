304fd6c9394004868845c856d31a6f7a4f913374
Pelayo Besa <pelayobesa+git@gmail.com>
2009-10-26 01:08:43 -0300

    - Moving the song list creation part of the template to a partial outer template. This will allow the same template to be used for both main page generation and ajax reloading.

--- kaariok/songs/views.py
+++ kaariok/songs/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
 from django.template import RequestContext
 
 from kaariok.songs.models import Song
 
 def song_search(request):
     songs = Song.objects.all().extra(select={'name_upper' : 'upper(name)'}).order_by('name_upper')
+    song_list_html = render_to_string('songs/partial/song_list.html',
+        {'songs' : songs},
+         context_instance=RequestContext(request)
+    )
     
     return render_to_response('songs/search.html',
     {
-        'songs' : songs,
+        'song_list_html' : song_list_html,
     },
     context_instance=RequestContext(request))
