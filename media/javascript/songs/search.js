cac28999f96c3187b6514bd45f6521e2acf3fc9e
Pelayo Besa <pelayobesa+git@gmail.com>
2009-10-31 08:51:58 -0300

    Song search: Extracted the song reload to a separate function.

--- media/javascript/songs/search.js
+++ media/javascript/songs/search.js
@@ -1,15 +1,19 @@
+var songlist_reload = function(){
+    var approved = $("#song_list_filters_approved").val()
+    $.getJSON(
+        "/song/search/",
+        {
+           'approved': approved
+        },
+        function(data){
+                $("#song_list_div").html(data['html']);
+        }
+    );
+}
+
 $(document).ready(function(){
     $("#song_list_filters_approved").change( function(){
-        //Login user here. With an indicator thar the user is changing.
-        $.getJSON(
-            "/song/search/",
-            {
-               'approved': $("#song_list_filters_approved").val()
-            },
-            function(data){
-                    $("#song_list_div").html(data['html']);
-            }
-        );
+        songlist_reload();
     }
     );
 });
