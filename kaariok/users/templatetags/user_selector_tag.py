13ff69d71ed0529ac3acb177d51d68f418dabbcb
Pelayo Besa <pelayobesa+git@gmail.com>
2009-11-05 00:44:14 -0300

    User selector tag fix.

--- kaariok/users/templatetags/user_selector_tag.py
+++ kaariok/users/templatetags/user_selector_tag.py
@@ -7,4 +7,5 @@ register = template.Library()
 @register.inclusion_tag('users/templatetags/user_selector.html')
 def user_selector(token):
     users = User.objects.all().extra(select={'username_upper': 'upper(username)'}).order_by('username_upper')
+    
     return {'users':users, 'selected_user':token}
