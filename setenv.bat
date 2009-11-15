029dbf3e415a67dc7226bae5390e2d8a222ec13c
unknown <Carlita@.(none)>
2009-11-15 11:28:48 -0300

    Add python path to file
    Winddoes dos not necesarilly have python
    in the path, so we add it to the environment
    bat to simplify running django

--- setenv.bat
+++ setenv.bat
@@ -2,4 +2,5 @@ set APP_ROOT=%cd%
 set DJANGO_SETTINGS_MODULE=kaariok.settings
 set PATH=%PATH%;%APP_ROOT%\lib\django\bin
 set PATH=%PATH%;%APP_ROOT%\lib\debug_toolbar
+set PATH=%PATH%;C:\python25
 set PYTHONPATH=%APP_ROOT%;%APP_ROOT%\lib
